ed96208a84d62f409df2b866a20c59dbeab82e7c
Nanyan6616 <3303354353@qq.com>
2025-11-14 17:44:23 +0800

    revise

--- week2/code/I2C.c
+++ week2/code/I2C.c
@@ -1,19 +1,27 @@
 #include "RM.h"
-
+#include "stm32f1xx_hal_conf.h"
 void start()
 {
     SDA_High();
     SCL_High();
+    HAL_Delay(5);
+
     SDA_Low();
+    HAL_Delay(5);
     SCL_Low();
+    HAL_Delay(5);
 }
 
 void stop()
 {
     SCL_Low();
     SDA_Low();
+    HAL_Delay(5);
+
     SCL_High();
+    HAL_Delay(5);
     SDA_High();
+    HAL_Delay(5);
 }
 
 void SendByte(char n)
@@ -21,12 +29,20 @@ void SendByte(char n)
     for(int i=0;i<8;i++)
     {
         SCL_Low();
+        HAL_Delay(5);
         if(n & 0x80)
+        {
         SDA_High();
+        HAL_Delay(5);
+        }
         else
+        {
         SDA_Low();
+        HAL_Delay(5);
+        }
         SCL_High();
         n = n<<1;
+        HAL_Delay(5);
     }
 }
 
@@ -34,8 +50,11 @@ char ReceiveAck()
 {
     char ack;
     SCL_Low();
+    HAL_Delay(5);
     SDA_High();
+    HAL_Delay(5);
     SCL_High();
+    HAL_Delay(5);
     if(SDA_Read())
     ack = 1;
     else
